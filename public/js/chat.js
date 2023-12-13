0c1033e5712294c201e8d435c7aa3480d404e8c6
220106 <220106rq@yse-c.net>
2023-12-13 11:39:59 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,17 @@
+// Chatサーバに接続
+const URL = "";
+const socket = io.connect(URL); 
+
 function sendMessage() {
-    console.log("sendMessage")
+    // console.log('send message!!')
+    var message = document.getElementById('message').value;
+    console.log(message);
+    if (message) {
+        var data = {
+            message:message
+        }
+        socket.emit('chat_message',data);
+        
+    }
 }
+
