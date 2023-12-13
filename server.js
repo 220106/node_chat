48a0193f25530d60318178bf13f5c450135ecda6
220106 <220106rq@yse-c.net>
2023-12-13 10:45:02 +0900

    update

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-
-//Expressサーバ作成
-const express = require('express')
-const app = express()
-
+// Express の作成
+const express = require('express');
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-//publicフォルダをweb公開する
+// publicフォルダを、Web公開
 app.use(express.static(__dirname + '/public'));
 
-//httpサーバ作成
+// HTTPサーバの作成
 const { createServer } = require('node:http');
 const server = createServer(app);
 
 //環境変数読み込み
 const dotenv = require('dotenv');
 dotenv.config();
-const host = process.env.HOST
-const port = process.env.PORT
+const host = process.env.HOST;
+const port = process.env.PORT;
 
-//httpサーバ待機
-server.listen(port, host, () => {
+// HTTPサーバー待機
+server.listen(port, host, ()=> {
     console.log(`listening on http://${host}:${port}`);
 })
