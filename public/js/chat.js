9ff826403051111d1651918a730243eb1b0ae7e2
220106 <220106rq@yse-c.net>
2023-12-13 11:56:16 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,16 +2,22 @@
 const URL = "";
 const socket = io.connect(URL); 
 
+socket.on('chat_message', (data) => {
+    console.log(data);
+    var p = document.createElement('p');
+    p.innerText = data.socketID + ": " + data.message;
+    document.getElementById('chat-list').prepend(p);
+})
+
 function sendMessage() {
     // console.log('send message!!')
     var message = document.getElementById('message').value;
     console.log(message);
     if (message) {
-        var data = {
-            message:message
+        //データを作成
+        var data = { 
+            message: message
         }
-        socket.emit('chat_message',data);
-        
+        socket.emit('chat_message', data);
     }
 }
-
